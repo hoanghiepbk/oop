7e5408bcbb96620926191e713222e5fc5534a083
Hoang Hiep <hoanghiepbk@gmail.com>
2017-10-04 16:04:59 +0700

    list

---
+++ student-list.php
@@ -0,0 +1,48 @@
+<?php
+
+require 'student_db.php';
+class student extends DB_student{
+	function __construct() 
+    {
+        parent::__construct();
+	} 
+}	
+$stu = new student ();
+
+?>
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Danh sách sinh vien</title>
+       
+    </head>
+    <body>
+        <h1>Danh sách sinh vien</h1>
+        <a href="student-add.php">Thêm sinh viên</a> <br/> <br/>
+        <table width="100%" border="2" cellspacing="0" cellpadding="15">
+            <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Gender</td>
+                <td>Birthday</td>
+                <td>Options</td>
+            </tr>
+            <?php foreach ($stu as $item){ ?>
+            <tr>
+                <td><?php echo $item['sv_id']; ?></td>
+                <td><?php echo $item['sv_name']; ?></td>
+                <td><?php echo $item['sv_sex']; ?></td>
+                <td><?php echo $item['sv_birthday']; ?></td>
+                <td>
+                    <form method="post" action="student-delete.php">
+                        <input onclick="window.location = 'student-edit.php?id=<?php echo $item['sv_id']; ?>'" type="button" value="Sửa"/>
+                        <input type="hidden" name="id" value="<?php echo $item['sv_id']; ?>"/>
+                        <input onclick="return confirm('Bạn có chắc muốn xóa không?');" type="submit" name="delete" value="Xóa"/>
+                    </form>
+                </td>
+            </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>
